13fc563b3bb445c21cf0c1a6bc72d6b008c06570
Matt <matt@primestudiosllc.com>
2012-04-18 02:47:34 -0700

    config

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env == "production"
-    provider :facebook, '326858757357937', '1370c9bef67b62532be4dbe516292f5d'
-  else
-    provider :facebook, '343838685637455', '2177c742e4bf8011bad450ce32d92eb8'
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Spinit::Application.config.secret_token = 'd989ca08a857f2a7729d6a788c0e1df44e3468d84011bfa57aabaf5994b99d416c1ce0399397a78607f2bb19976a9daf3e561ea07fb60f5872dabca731a09a66'
